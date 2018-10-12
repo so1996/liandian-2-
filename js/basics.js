0aa3d2776d7d7d10c466e84b0d400cc174cf63bd
hugh <X_shao_nian@foxmail.com>
2018-10-12 21:42:17 +0800

    联点项目第二版 缓冲动画完成

--- js/basics.js
+++ js/basics.js
@@ -13,9 +13,6 @@
 
     $(window).scroll(function () {
         startAnimate();
-
-        bufferExercise();
-
     });
 
     start_animate.on("click", function () {
@@ -40,62 +37,62 @@
         }
     }
 
-    var p=0,
-        t=0;
-    $(window).scroll(function(){
-        p=$(this).scrollTop();
-        t<p? flag=true : flag=false;
-        setTimeout(function(){ t = p ; },0)
-    });
-
-    function bufferExercise() {
-        //声明定时器运行次数
-        var index = -1;
-        //声明步长值step
-        var step;
-        //声明当前值cur
-        var cur, parameterString, i;
-        //声明目标值
-        var target = parseFloat('83');
-
-        clearInterval(two_1.timer);
-        two_1.timer = setInterval(function () {
-            //更新定时器的工作次数
-            index++;
-            //更新步长值
-            step = 2 - (3 * index + 1) / target;
-            //更新当前值
-            if (getCSS(two_1[0], 'transform') !== "none") {
-                parameterString = getCSS(two_1[0], 'transform').split(',')[5];
-                cur = parseFloat(parameterString.slice(0, parameterString.length - 1));
-            } else {
-                cur = 0;
-            }
-
-            if(flag){
-                i = cur + step;
-            }else{
-                i = cur - step;
-            }
-
-            //若步长设置值使得元素超过目标点时，将步长设置值更改为目标点值 - 当前值
-            // if((cur+step-target)*step>0){
-            //     step = target - cur;
-            // }
-
-            // console.log(i);
-            //更新left值
-            two_1.css({transform: "matrix(1,0,0,1,0," + i + ")"});
-
-            //当元素到达目标点时，停止定时器
-            // if(step === target - cur){
-            //     clearInterval(two_1.timer);
-            // }
-
-            if (step < 0) {
-                clearInterval(two_1.timer);
-            }
-        }, 80);
-    }
+    //var p=0,
+    //    t=0;
+    //$(window).scroll(function(){
+    //    p=$(this).scrollTop();
+    //    t<p? flag=true : flag=false;
+    //    setTimeout(function(){ t = p ; },0)
+    //});
+    //
+    //function bufferExercise() {
+    //    //声明定时器运行次数
+    //    var index = -1;
+    //    //声明步长值step
+    //    var step;
+    //    //声明当前值cur
+    //    var cur, parameterString, i;
+    //    //声明目标值
+    //    var target = parseFloat('83');
+    //
+    //    clearInterval(two_1.timer);
+    //    two_1.timer = setInterval(function () {
+    //        //更新定时器的工作次数
+    //        index++;
+    //        //更新步长值
+    //        step = 2 - (3 * index + 1) / target;
+    //        //更新当前值
+    //        if (getCSS(two_1[0], 'transform') !== "none") {
+    //            parameterString = getCSS(two_1[0], 'transform').split(',')[5];
+    //            cur = parseFloat(parameterString.slice(0, parameterString.length - 1));
+    //        } else {
+    //            cur = 0;
+    //        }
+    //
+    //        if(flag){
+    //            i = cur + step;
+    //        }else{
+    //            i = cur - step;
+    //        }
+    //
+    //        //若步长设置值使得元素超过目标点时，将步长设置值更改为目标点值 - 当前值
+    //        // if((cur+step-target)*step>0){
+    //        //     step = target - cur;
+    //        // }
+    //
+    //        // console.log(i);
+    //        //更新left值
+    //        two_1.css({transform: "matrix(1,0,0,1,0," + i + ")"});
+    //
+    //        //当元素到达目标点时，停止定时器
+    //        // if(step === target - cur){
+    //        //     clearInterval(two_1.timer);
+    //        // }
+    //
+    //        if (step < 0) {
+    //            clearInterval(two_1.timer);
+    //        }
+    //    }, 80);
+    //}
 
 })($);
